159b6c4bdb7b0716f79a7606bf0452d6549d14ec
DalerKhusainov <dalerkhusainov85@gmail.com>
2024-08-22 23:12:21 +0500

    Added navbar, home page and implemented animation for switching pages.

--- components/Nav.js
+++ components/Nav.js
@@ -32,11 +32,28 @@ const Nav = () => {
   const router = useRouter();
   const pathname = router.pathname;
   return (
-    <nav>
-      <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-80 bg-red-200/10 xl:h-max">
+    <nav className="fixed bottom-0 flex flex-col items-center xl:justify-center gap-y-4 h-max mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen">
+      <div className="flex items-center justify-between w-full px-4 py-8 text-3xl xl:flex-col xl:justify-center gap-y-10 md:px-40 xl:px-0 h-[80px] xl:h-max bg-white/10 backdrop-blur-sm xl:text-xl xl:rounded-full">
         {navData.map((link, index) => (
-          <Link key={index} href={link.path}>
-            {link.icon}
+          <Link
+            className={`${
+              pathname === link.path && "text-accent"
+            } relative flex items-center group hover:text-accent transition-all duration-300`}
+            key={index}
+            href={link.path}
+          >
+            {/* tooltip */}
+            <div className="absolute right-0 hidden pr-14 xl:group-hover:flex">
+              <div className="bg-white relative flex text-primary items-center p-[6px] rounded-[3px]">
+                <div className="text-[12px] leading-none font-semibold capitalize">
+                  {link.name}
+                </div>
+                {/* triangle */}
+                <div className="border-solid border-l-white border-l-8 border-y-transparent border-y-[6px] border-r-0 absolute -right-2"></div>
+              </div>
+            </div>
+            {/* icon */}
+            <div>{link.icon}</div>
           </Link>
         ))}
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,72 @@
+import Image from "next/image";
+import ParticlesContainer from "../components/ParticlesContainer";
+import ProjectsBtn from "../components/ProjectsBtn";
+import Avatar from "../components/Avatar";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="h-full bg-primary/60">
+      {/* text */}
+      <div className="w-full h-full bg-gradient-to-t from-primary/10 via-black/30 to-black/10">
+        <div className="container flex flex-col justify-center h-full mx-auto text-center xl:pt-40 xl:text-left">
+          {/* title */}
+          <motion.h1
+            variants={fadeIn("down", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1"
+          >
+            Воплощение Идей в<br />{" "}
+            <span className="text-accent">Цифровую Реальность</span>
+          </motion.h1>
+          {/* subtitle */}
+          <motion.p
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm mx-auto mb-10 xl:max-w-xl xl:mx-0 xl:mb-16"
+          >
+            Разработка современных веб-решений с учетом ваших потребностей
+          </motion.p>
+          {/* btn */}
+          <div className="relative flex justify-center xl:hidden">
+            <ProjectsBtn />
+          </div>
+          <motion.div
+            variants={fadeIn("down", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden xl:flex"
+          >
+            <ProjectsBtn />
+          </motion.div>
+        </div>
+      </div>
+      {/* image */}
+      <div className="w-[1200px] h-full absolute right-0 bottom-0">
+        {/* bg img */}
+        <div className="absolute w-full h-full bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat mix-blend-color-dodge translate-z-0"></div>
+        {/* particles */}
+        <ParticlesContainer />
+        {/* avatar img */}
+        <motion.div
+          variants={fadeIn("up", 0.5)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          transition={{ duration: 1, ease: "easeInOut" }}
+          className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]"
+        >
+          <Avatar />
+        </motion.div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
