cd3e49fedc8a3fd193b28241798f661ef2cd6e30
DalerKhusainov <dalerkhusainov85@gmail.com>
2024-08-28 11:25:39 +0500

    Added favicon and changed the title of website.

--- components/Avatar.js
+++ components/Avatar.js
@@ -5,8 +5,8 @@ const Avatar = () => {
     <div className="hidden xl:flex xl:max-w-none">
       <Image
         src="/avatar_img.png"
-        width={737}
-        height={638}
+        width={678}
+        height={678}
         alt=""
         className="w-full h-full translate-y-10"
       />
